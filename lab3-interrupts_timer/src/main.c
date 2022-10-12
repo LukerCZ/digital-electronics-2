101ebb4580032a55cf360575b09ceeaaf0c6aa8b
Lukáš Kudrna <kudrnalukas209@gmail.com>
2022-10-12 10:40:57 +0200

    [LAB3]

--- lab3-interrupts_timer/src/main.c
+++ lab3-interrupts_timer/src/main.c
@@ -2,19 +2,52 @@
 #include <gpio.h>           // GPIO library for AVR-GCC
 #include "timer.h"          // Timer library for AVR-GCC
 
+#define LED_GREEN PB5
+#define LED_RED PB0
+
 int main(void)
 {
-    ...
+    GPIO_mode_output(&DDRB, LED_GREEN);  
+    GPIO_mode_output(&DDRB, LED_RED); 
     // Enable overflow interrupt
+    TIM1_overflow_1s();
     TIM1_overflow_interrupt_enable();
-    ...
+    
+    TIM0_overflow_4ms();
+    TIM0_overflow_interrupt_enable();
     // Enables interrupts by setting the global interrupt mask
     sei();
-    ...
+    
+    while (1)
+    {
+    
+    }
 }
 
 // Interrupt service routines
 ISR(TIMER1_OVF_vect)
 {
-    ...
+   PORTB = PORTB ^ (1<<LED_GREEN);
+  
+}
+/*ISR(TIMER0_OVF_vect)
+{
+   
+   PORTB = PORTB ^ (1<<LED_RED);
+}*/
+
+ISR(TIMER0_OVF_vect)
+{
+    static uint8_t no_of_overflows = 0;
+
+    no_of_overflows++;
+    if (no_of_overflows >= 4)
+    {
+      PORTB = PORTB ^ (1<<LED_RED);   
+        // Do this every 6 x 16 ms = 100 ms
+        no_of_overflows = 0;
+        
+    }
+    TCNT0 = 128;
+    // Else do nothing and exit the ISR
 }
