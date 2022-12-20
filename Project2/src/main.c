284aaeb63a3b12531918a29090b0b71fb7a09c40
Martin Kováč <99388246+KovacM1@users.noreply.github.com>
2022-12-20 22:22:43 +0100

    Update main.c

--- Project2/src/main.c
+++ Project2/src/main.c
@@ -1,19 +1,5 @@
-/***********************************************************************
- * 
- *
- * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
- * 
- *
-/* Define pins for motor 1 ------------------------------------------*/
-#define servo1 PB1 
-
-/* Define pins for motor 2 ---------------------------------------------*/
-#define servo2 PB2
-
-/* Define pins for button ---------------------------------------------*/
-#define BUTTON1 PB0          // Button 1
-
 /* Includes ----------------------------------------------------------*/
+#include "Arduino.h"
 #include <avr/io.h>         // AVR device-specific IO definitions
 #include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
 #include <util/delay.h>     // Functions for busy-wait delay loops
@@ -21,54 +7,45 @@
 #include "timer.h"          // Timer library for AVR-GCC
 #include <stdlib.h>         // C library. Needed for number conversions
 
-/* Arduino world -----------------------------------------------------*/
-#include "Arduino.h"
+// Define pins for servo
+#define servo1 PB1 
+#define servo2 PB2
 
-/* Define parameters for servo ---------------------------------------------------------*/
-# define ZERO_ANGLE 124
-# define ANGLE_90 260
-# define SHORT_DELAY 2000 // Delay in milliseconds
+// JOYSTICK button
+#define button PB0 
 
-volatile uint32_t servo1_position = ANGLE_90;
-volatile uint32_t servo2_position = ANGLE_90;
-volatile uint8_t MOVE = 0;
+// Servo default values
+# define Servo_zero 124
+# define Servo_90 260
+
+uint32_t servo1_position = Servo_90;
+uint32_t servo2_position = Servo_90;
+uint8_t servo_move = 0;
 
 int main(void)
 {
-    /* ----------------------------- Timer for SERVO 1 ---------------------------------*/
-    // Configure 8-bit Timer/Counter0 to control servo motors
-    // Set prescaler to 16 ms and enable overflow interrupt
-    TIM0_overflow_16ms();
-    TIM0_overflow_interrupt_enable();
+  //Set pins with gpio library
+  GPIO_mode_output(&DDRB, servo1);        // First servo motor
+  GPIO_mode_output(&DDRB, servo2);        // Second servo motor
+  GPIO_mode_input_pullup(&DDRB,button);  // Button with enabled pull-up resistor
 
-    /* ------------------------- Set pins with gpio library ----------------------------*/
-    GPIO_mode_output(&DDRB, servo1);        // First servo motor
-    GPIO_mode_output(&DDRB, servo2);        // Second servo motor
-    GPIO_mode_input_pullup(&DDRB,BUTTON1);  // Button with enabled pull-up resistor
+  // Set PWM registers, PWM mode
+  TCCR1A &= ~((1 << COM1A0) | (1 << COM1B0));
+  TCCR1A |= (1 << WGM11) | (1 << COM1A1) | (1 << COM1B1);
+  TCCR1B |= (1 << WGM13);
+  // PWM period
+  ICR1 = 20000;
+  OCR1A = servo1_position;
+  OCR1B = servo2_position;
+  // Set prescalar
+  TCCR1B |= (1 << CS11);
 
-    // Set 10. waveform generation mode (1010)
-    TCCR1A |= (1 << WGM11);                  
-    TCCR1B |= (1 << WGM13);
 
-    // Set compare output mode
-    TCCR1A |= (1 << COM0A1) | (1 << COM0B1); 
+  // Set timer 0, enable interrupt
+  TIM0_overflow_1ms();
+  TIM0_overflow_interrupt_enable();
 
-     // Set TOP value
-    ICR1 = 2500;
-                         
-    // Set duty cycle
-    OCR1A = servo1_position;
-    OCR1B = servo2_position;
-    
-    // Set prescaler to 64
-    TCCR1B |= (1 << CS11) | (1 << CS10 ); 
-
-    PCICR |= (1<<PCIE0);                    // Any change of any enable PCINT[7:0] pins will cause an interrupt
-    PCMSK0 |= (1<<PCINT0);                  // Enable PCINT0 change interrupt  
-    _delay_ms(SHORT_DELAY); // Wait 2 s
-
-    // Enables interrupts by setting the global interrupt mask
-    sei();
+  sei();
     
     // Infinite loop
     while (1)
@@ -82,80 +59,75 @@ int main(void)
 
 }
 
-/* Interrupt service routines ----------------------------------------*/
-/**********************************************************************
- * Function: Timer/1 overflow interrupt
- * Purpose:  Use single conversion mode and start conversion every 33 ms.
- **********************************************************************/
 
 ISR(TIMER0_OVF_vect)
 {
-    static uint8_t servo1_direction = 0;        // Default direction setup for first servo motor
-    static uint8_t servo2_direction = 0;        // Default direction setup for second servo motor
-    /* ------------- First servo motor  ------------------------------*/
-    if (MOVE == 0)                                                      
+    //Setup default direction for servo
+    static uint8_t servo1_direction = 0;        
+    static uint8_t servo2_direction = 0;
+   
+   //Servo 1
+    if (servo_move == 0)                                                      
     {
         // Direction
-        if (servo1_position == ANGLE_90)       // Maximum value
+        if (servo1_position == Servo_90)       
             {
-                servo1_direction = 1;           // Clockwise direction
+                servo1_direction = 1;           
             }
-        else if (servo1_position == ZERO_ANGLE) // Minimum value
+        else if (servo1_position == Servo_zero) 
             {
-                servo1_direction = 0;           // Counterclockwise direction
+                servo1_direction = 0;           
             }
         // Motion
         if (servo1_direction == 0)
         {
-            servo1_position += 2;               // Increment variable 
+            servo1_position += 2;               
         }
         if (servo1_direction == 1)
         {
-            servo1_position -= 2;               // Decrement variable  
+            servo1_position -= 2;                 
         }
-        OCR1A = servo1_position;                // Put value from variable into arduino register
+        OCR1A = servo1_position;                
         }
-    /* ------------- Second servo motor  ------------------------------*/    
-    if (MOVE == 1) 
+
+    //Servo 2    
+    if (servo_move == 1) 
     {
         // Direction
-        if (servo2_position == ANGLE_90)       // Maximum value
+        if (servo2_position == Servo_90)       
         {
-            servo2_direction = 1;               // Clockwise direction
+            servo2_direction = 1;               
         }
-        else if (servo2_position == ZERO_ANGLE) // Minimum value
+        else if (servo2_position == Servo_zero) 
         {
-            servo2_direction = 0;               // Counterclockwise direction
+            servo2_direction = 0;               
         }
         // Motion
         if (servo2_direction == 0)
         {
-            servo2_position += 2;               // Increment variable 
+            servo2_position += 2;                
         }
         if (servo2_direction == 1)
         {
-            servo2_position -= 2;               // Decrement variable   
+            servo2_position -= 2;                  
         }
-        OCR1B = servo2_position;                // Put value from variable into arduino register
+        OCR1B = servo2_position;                
     }
 }
-/**********************************************************************
- * Function: Pin PB0 change interrupt
- * Purpose:  Defines if the first or second servo motor should move
- **********************************************************************/
 
+//Define which servo motor should move
 ISR(PCINT0_vect)
 {
-    uint8_t sw1 = GPIO_read(&PINB, BUTTON1);     // Read value from digital pin 8
-    if (sw1 == LOW)                              // Button is pushed down  
+    uint8_t sw1 = GPIO_read(&PINB, button);     
+    if (sw1 == LOW)                                
     {
-        if (MOVE == 1) 
+        if (servo_move == 1) 
         { 
-            MOVE = 0;                            
+            servo_move = 0;                            
         }
-        else if (MOVE == 0) 
+        else if (servo_move == 0) 
         { 
-            MOVE = 1;
+            servo_move = 1;
         }
     } 
 }
